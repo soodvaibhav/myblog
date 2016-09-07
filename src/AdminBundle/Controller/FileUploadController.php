4e98c1ba2c5599dca68f36740a9acca4aae30d43
Vaibhav Sood <vaibhavsood77@gmail.com>
2016-09-07 19:08:29 +0000

    File upload

--- src/AdminBundle/Controller/FileUploadController.php
+++ src/AdminBundle/Controller/FileUploadController.php
@@ -6,8 +6,16 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-Class UploadFile
+Class FileUploadController
 {
+    private $imgDomain, $imgPath;
+
+    public function __construct($imgDomain, $imgPath)
+    {
+        $this->imgDomain = $imgDomain;
+        $this->imgPath = $imgPath;
+    }
+
     public function indexAction(Request $request)
     {
         $files = $request->files->get('files');
@@ -16,16 +24,15 @@ Class UploadFile
             throw new NotFoundHttpException('Error Uploading Image');
         }
         $status = ['files' => [
-            ['url' => $request->headers->get('origin') . $fileName ]
+            ['url' => $this->imgDomain . $fileName ]
         ]];
         return new JsonResponse($status);
     }
 
     public function upload(UploadedFile $file)
     {
-        $location = '/home/vagrant/myblog/web';
-        $path = '/images/' . date('Y') . '/' . date('m') . '/';
-        $directory = $location . $path;
+        $path = date('Y') . '/' . date('m') . '/' . 'original' . '/';
+        $directory = $this->imgPath . $path;
         if (!is_dir($directory)) {
             mkdir($directory, 0755, true);
         }
